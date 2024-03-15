60a56c094bed9d1367c9031baa3c6b43baa99059
Samir Echavarria <juan.urregoe@campusucc.edu.co>
2024-03-14 20:50:55 -0500

    Progreso

--- lib/pantallas/headline.dart
+++ lib/pantallas/headline.dart
@@ -6,7 +6,12 @@ class Headline extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-    
+    child: Column
+    (children: [
+      Text("Mercados campesinos a tu alcance"), 
+      Text("Explora nuestro catálogo y empieza a llenar tu alacena de compras con los productos más deliciosos y saludables."),
+      AssetImage("assets/imagenes/foto1.png")
+    ],),
     );
   }
 }
