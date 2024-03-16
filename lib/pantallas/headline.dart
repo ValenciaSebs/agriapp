aa68ae50a3f3b1178a1a0567a03a0c3e73bfdb2b
Samir Echavarria <juan.urregoe@campusucc.edu.co>
2024-03-15 19:13:40 -0500

    App Funcionando

--- lib/pantallas/headline.dart
+++ lib/pantallas/headline.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Headline extends StatelessWidget {
   Headline({super.key});
@@ -9,9 +13,39 @@ class Headline extends StatelessWidget {
     return Container(
     child: Column
     (children: [
-      Text("Mercados campesinos a tu alcance"), 
-      Text("Explora nuestro catálogo y empieza a llenar tu alacena con los productos más deliciosos y saludables."),
-      Image.asset("assets/imagenes/foto1.png")
+
+      Container(padding:EdgeInsets.only(left: 100, right: 100),
+        child: Text(
+      'Mercados campesinos a tu alcance',
+      textAlign: TextAlign.center,
+      style: GoogleFonts.encodeSansExpanded(
+      textStyle: Theme.of(context).textTheme.displayLarge, 
+      fontSize: 25,
+      fontWeight: FontWeight.w600,
+      fontStyle: FontStyle.normal,
+      ),
+      ),
+      ),
+      
+
+      Container(padding: EdgeInsets.only(left: 90, right: 90),
+      child: Text(
+      'Explora nuestro catálogo y empieza a llenar tu alacena con los productos más deliciosos y saludables.',
+      textAlign: TextAlign.center,
+      style: GoogleFonts.encodeSansExpanded(
+      textStyle: Theme.of(context).textTheme.displayLarge,
+      fontSize: 15,
+      fontWeight: FontWeight.w400,
+      ),
+      ),
+      ),
+
+      Container(
+      child: 
+      Image.asset("assets/imagenes/foto1.png"),
+      ),
+  
+      
     ],),
     );
   }
