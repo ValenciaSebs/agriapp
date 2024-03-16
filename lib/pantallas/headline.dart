8ba9953745ddcab60b2ad400ab25745815f364c5
Samir Echavarria <juan.urregoe@campusucc.edu.co>
2024-03-15 19:55:04 -0500

    Headline Completo

--- lib/pantallas/headline.dart
+++ lib/pantallas/headline.dart
@@ -9,7 +9,7 @@ class Headline extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Container(color: Color(0xFF0d5846), padding: EdgeInsets.only(bottom: 15),
     child: Column
     (children: [
 
@@ -18,23 +18,26 @@ class Headline extends StatelessWidget {
       'Mercados campesinos a tu alcance',
       textAlign: TextAlign.center,
       style: GoogleFonts.encodeSansExpanded(
-      textStyle: Theme.of(context).textTheme.displayLarge, 
+      textStyle: Theme.of(context).textTheme.displayLarge, height: 1.2,
       fontSize: 25,
       fontWeight: FontWeight.w600,
       fontStyle: FontStyle.normal,
+      color: Color(0xFF80a892),
       ),
       ),
       ),
       
 
-      Container(padding: EdgeInsets.only(left: 55, right: 55),
+      Container(padding: EdgeInsets.only(left: 65, right: 65),
       child: Text(
       'Explora nuestro catálogo y empieza a llenar tu hogar y tu cocina con los productos más deliciosos y saludables.',
       textAlign: TextAlign.center,
       style: GoogleFonts.encodeSansExpanded(
-      textStyle: Theme.of(context).textTheme.displayLarge, height: 1.5,
-      fontSize: 17,
+      textStyle: Theme.of(context).textTheme.displayLarge, height: 1.4,
+      fontSize: 16,
       fontWeight: FontWeight.w400,
+      fontStyle: FontStyle.normal,
+      color: Color(0xFFfdf2e3),
       ),
       ),
       ),
