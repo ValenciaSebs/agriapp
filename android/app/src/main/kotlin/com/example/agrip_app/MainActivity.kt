105031699dc63ea47308f4d115df3fe3edaf3aa5
Joseph-Hernande <josephjafir12@gmail.com>
2024-04-17 19:08:15 -0500

    Actualizacion

--- android/app/src/main/kotlin/com/example/agrip_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.agrip_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -5,18 +5,15 @@ import 'package:agrip_app/pantallas/inicio.dart';
 import 'package:agrip_app/pantallas/iniciosin.dart';
 import 'package:agrip_app/pantallas/noticias.dart';
 import 'package:agrip_app/pantallas/panperfil.dart';
-import 'package:agrip_app/pantallas/panperfilsin.dart';    
+import 'package:agrip_app/pantallas/panperfilsin.dart';
 import 'package:agrip_app/pantallas/regisingres.dart';
 import 'package:agrip_app/pantallas/registro.dart';
 import 'package:agrip_app/pantallas/splash_screen.dart';
 import 'package:agrip_app/pantallas/usuario2.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart'; // Importa Firebase Core
 
-//void main() {
-//  runApp(Inicio());
-//}e
-
-Future main() async {
+/*Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -26,5 +23,25 @@ Future main() async {
       'home': (context) => Regisingres(),
     },
   ));
-  //await Firebase.initializeAPP();
+}*/
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(); // Inicializa Firebase
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'splash',
+      routes: {
+        'splash': (context) => SplashScreen(),
+        'home': (context) => Regisingres(),
+        // Agrega tus rutas de navegación adicionales aquí
+      },
+    );
+  }
 }
