92ac24759ab47c81247a8188514800518f4449bc
Joseph-Hernande <josephjafir12@gmail.com>
2024-04-17 19:15:09 -0500

    Actualizacion

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,7 @@
-import 'package:agrip_app/pantallas/actuausuario.dart';
-import 'package:agrip_app/pantallas/headline.dart';
-import 'package:agrip_app/pantallas/ingreso.dart';
-import 'package:agrip_app/pantallas/inicio.dart';
-import 'package:agrip_app/pantallas/iniciosin.dart';
-import 'package:agrip_app/pantallas/noticias.dart';
-import 'package:agrip_app/pantallas/panperfil.dart';
-import 'package:agrip_app/pantallas/panperfilsin.dart';
 import 'package:agrip_app/pantallas/regisingres.dart';
-import 'package:agrip_app/pantallas/registro.dart';
 import 'package:agrip_app/pantallas/splash_screen.dart';
-import 'package:agrip_app/pantallas/usuario2.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart'; // Importa Firebase Core
-
-/*Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    initialRoute: 'splash',
-    routes: {
-      'splash': (context) => SplashScreen(),
-      'home': (context) => Regisingres(),
-    },
-  ));
-}*/
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
